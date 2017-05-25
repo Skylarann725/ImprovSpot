f91a972a501922f5705d2ddcf97d9977976472e1
bear9151 <barrett.allen@icloud.com>
2017-05-24 20:16:15 -0400

    package.json and fixed schema file with avg_rating fixed

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE `gameinfo` (
     game_name VARCHAR(255) NOT NULL,
     instructions VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
-    avg_rating FLOAT(255) DEFAULT NULL,
+    avg_rating FLOAT(5,4) DEFAULT NULL,
    	min_players TINYINT NOT NULL,
    	max_players TINYINT DEFAULT NULL,
   	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
